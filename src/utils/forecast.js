98f1a66a794d34764d7a478b241b1171fa8c0699
Aleksejs.Pcelincevs <aleksejs.pcelincevs@gmail.com>
2019-03-28 19:49:14 +0200

    Added high/low temperatures

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '. There is a ' + body.currently.precipProbability + '% chance of rain. Highest temperature ' + body.daily.data[0].temperatureHigh + '. Lowest temperature ' + body.daily.data[0].temperatureLow + '.')
         }
     })
 }
